7413aac3e842e6b9025777f228196f11ae131a46
JamshedSadullaev <sadullayev98@gmail.com>
2022-09-08 20:02:32 -0400

    adding new api codes

---
+++ controllers/api/index.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const quotesRouter = require("./quotesRoutes");
+
+router.use("/user",quotesRouter);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sequelize = require("./config/connection");
+const controllers = require ("./controllers");
+const { User, Quote } = require ("./models");
 const randomquotes = require("./controllers/api/quotesRoutes");
 
-app.use(randomquotes);
-app.listen(PORT, () => console.log("we're doing it"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/", async (req, res) => {
+  res.send("Working");
+});
+
+app.use(randomquotes);
+
+app.use(controllers);
+sequelize.sync({force:true}).then(()=>{
+  app.listen(PORT, () => console.log("we're doing it"));
+});
