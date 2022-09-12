c197fe03f4197ad29987e44ab48d435a4a568f17
Mavsic <muminova.m@hotmail.com>
2022-09-12 11:39:31 -0400

    time has been fixed

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({helpers});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,9 +16,10 @@ const randomquotes = require("./controllers/api/quotesRoutes");
 app.use(session(SessionConfig));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, 'public')));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.get("/", async (req, res) => {
 
