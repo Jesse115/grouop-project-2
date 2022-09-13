79dac344fa1ce176a3c4b608ecb127650f6a4133
JamshedSadullaev <sadullayev98@gmail.com>
2022-09-13 18:55:23 -0400

    saving indexjs

---
+++ utils/index.js
@@ -0,0 +1,11 @@
+// const session = require("express-session")
+
+module.exports = {
+  author: (req, res, next) => {
+    if ((req.session.loggedIn)) {
+      next();
+    } else {
+      res.status(500).json({ message: "Must be logged in" });
+    }
+  },
+};
