c988424fd982a155871a7c2dc107bac020229102
Jesse <ftsjess@gmail.com>
2022-09-15 18:53:08 -0400

    login works now

--- controllers/html/home.js
+++ controllers/html/home.js
@@ -2,17 +2,31 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   const loggedIn = req.session.loggedIn;
-  res.render("homepage",{
+  res.render("login", {
     loggedIn,
   });
 });
 router.get("/login", async (req, res) => {
   const loggedIn = req.session.loggedIn;
-  res.render("login",{
+  res.render("login", {
     loggedIn,
   });
 });
 router.get("/quotes", (req, res) => {
   res.render("quotes");
 });
+router.get("/diary", async (req, res) => {
+  const loggedIn = req.session.loggedIn;
+  if (loggedIn) {
+    res.render("homepage", {
+      loggedIn,
+    });
+  }
+  res.render("login", {
+    loggedIn,
+  });
+});
+
+
+
 module.exports = router;
