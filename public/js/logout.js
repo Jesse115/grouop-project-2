b52c6da71a301878e0b82c2dde110e6a3e828676
JamshedSadullaev <sadullayev98@gmail.com>
2022-09-15 11:01:19 -0400

    uer route and login handlebars changed

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,15 +1,17 @@
 const logoutElement = document.querySelector("#logout");
+
 const handleLogout = async (event) => {
   event.preventDefault();
-  const response =await fetch("/api/user/logout",{
+  const response = await fetch("/api/user/logout", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
   });
+
   if (response.ok) {
     document.location.replace("/");
   } else {
-    alert("Failed to log out.");
+    alert("Failed to logout");
   }
-  console.log("Logout");
 };
+
 logoutElement.addEventListener("click", handleLogout);
